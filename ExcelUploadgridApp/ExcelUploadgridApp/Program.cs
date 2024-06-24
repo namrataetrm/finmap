071bbfa10417d8058591bbec5a8484e82aceb5c6
siddhesh <sidpgdac@gmail.com>
2024-06-24 12:44:32 +0530

    backend updated (openxml added)

--- ExcelUploadgridApp/ExcelUploadgridApp/Program.cs
+++ ExcelUploadgridApp/ExcelUploadgridApp/Program.cs
@@ -1,20 +1,66 @@
+//using ExcelUploadgridApp.Data;
+//using Microsoft.EntityFrameworkCore;
+//using OfficeOpenXml;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAll", builder =>
+//    {
+//        builder.AllowAnyOrigin()
+//               .AllowAnyMethod()
+//               .AllowAnyHeader();
+//    });
+//});
+
+//var app = builder.Build();
+//app.UseCors("AllowAll");
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
 using ExcelUploadgridApp.Data;
 using Microsoft.EntityFrameworkCore;
-using OfficeOpenXml;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExcelUploadGridApp API", Version = "v1" });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 
 builder.Services.AddCors(options =>
 {
@@ -27,12 +73,14 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
 app.UseCors("AllowAll");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ExcelUploadGridApp API v1"));
 }
 
 app.UseHttpsRedirection();
@@ -42,3 +90,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
